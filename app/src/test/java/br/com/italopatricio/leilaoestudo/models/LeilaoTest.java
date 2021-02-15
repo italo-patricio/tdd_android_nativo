dcceee6a6d7bc899019f75201e2695205dbc99a5
Italo Patricio <elo7-developer+italo-patricio@elo7.com>
2021-02-15 18:26:36 -0300

    escrevendo comentario para os testes sem usar boas praticas

--- app/src/test/java/br/com/italopatricio/leilaoestudo/models/LeilaoTest.java
+++ app/src/test/java/br/com/italopatricio/leilaoestudo/models/LeilaoTest.java
@@ -18,6 +18,7 @@ public class LeilaoTest {
 
     @Test
     public void getMaiorLance() {
+        // verifica se devolve maior lance com apenas um lance
         Leilao console = new Leilao("Console");
         console.propoe(new Lance(new Usuario("Italo"), 200));
 
@@ -25,6 +26,7 @@ public class LeilaoTest {
 
         assertEquals(200.00, maiorLanceDevolvidoDoConsole, 0.0001);
 
+        // verifica se devolve mior lance com mais de um lance em ordem cerescente
         Leilao computador = new Leilao("Computador");
         computador.propoe(new Lance(new Usuario("Italo"), 100.0));
         computador.propoe(new Lance(new Usuario("Chico"), 200.0));
@@ -33,6 +35,7 @@ public class LeilaoTest {
 
         assertEquals(200.0, maiorLanceDevolvidoDoComputador, 0.0001);
 
+        // verifica se devolve maior lance com mais de um lance me ordem descrecente
         Leilao carro = new Leilao("Carro");
         carro.propoe(new Lance(new Usuario("Italo"), 10000.0));
         carro.propoe(new Lance(new Usuario("Chico"), 9000.0));
@@ -40,7 +43,6 @@ public class LeilaoTest {
         double maiorLanceDevolvidoDoCarro = carro.getMaiorLance();
 
         assertEquals(10000.0, maiorLanceDevolvidoDoCarro, 0.0001);
-        
     }
 
 }
